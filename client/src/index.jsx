38da218ceca25d56cedebb4e92fb1ffcd6df5f72
Giovaniavs <gavs2@cin.ufpe.br>
2020-07-05 17:10:39 -0300

    rota api/banner

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Banner from './components/api/banner/Banner';
 
 function App() {
   return(
     <div className="app">
-      <Banner />
     </div>
   )
 }
