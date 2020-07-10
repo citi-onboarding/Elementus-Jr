0ce257a9ac98c5cfd78ed5ca9d611c332803c08b
labprogsam <som3@cin.ufpe.br>
2020-07-10 14:36:14 -0300

    Feature(banner): Adjusted responsivity

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -21,15 +21,18 @@ function Banner() {
   return (
     <div>
       {Banner?.map(({ _id, titulo, descricao, imagem}) => (
-        <div key={_id}>
-          <div className="Banner">  
-            <img src= {imagem[0]?.url} className="Banner-image"/>
+        <div className="main-content" key={_id}>
+          <div className="container">
+
           </div>
           <div className="Texts">
             <p className="Title">{titulo}</p>
             <p className="Description">{descricao}</p>
             <Button Title="Contate-nos" buttonColor="#00005A" buttonSize= "180px"/>
-          </div>  
+          </div>
+          <div className="Banner">  
+            <img src= {imagem[0]?.url} className="Banner-image"/>
+          </div>
         </div>
         ))}
     </div>
