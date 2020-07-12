b16426f7f9652639484a08082763f9534b9f0c1f
Giovaniavs <gavs2@cin.ufpe.br>
2020-07-11 23:23:58 -0300

    barra lateral feita e funcao do botao sanduiche

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -25,9 +25,25 @@ function Navbar(){
     }
 
     function lateralNavbar() {
-        const secondBackground = document.getElementById("secondBackground")
-        secondBackground.classList.remove("secondBackground")
-        secondBackground.classList.add("secondBackgroundActived")
+        const secondBackground = document.getElementById("secondBackground");
+        const lateralNavbar = document.getElementById("lateralNavbarId");
+        if(!(secondBackground.classList.contains("secondBackgroundActived"))){
+            secondBackground.classList.remove("secondBackground");
+            secondBackground.classList.add("secondBackgroundActived");
+            lateralNavbar.classList.remove("lateralNavbar");
+            lateralNavbar.classList.add("lateralNavbarActived");
+        }
+        else if(secondBackground.classList.contains("secondBackgroundActived") && (secondBackground.classList.length === 1) ){
+            secondBackground.classList.remove("secondBackgroundActived");
+            secondBackground.classList.add("secondBackground");
+            lateralNavbar.classList.remove("lateralNavbarActived");
+            lateralNavbar.classList.add("lateralNavbar");
+        }
+        else if(secondBackground.classList.length === 2){
+            secondBackground.classList.remove("secondBackground")
+            lateralNavbar.classList.remove("lateralNavbar");
+            lateralNavbar.classList.add("lateralNavbarActived");
+        };
     };
 
     return(
@@ -42,18 +58,15 @@ function Navbar(){
                 <li><a className="link" href="#servicos">Serviços</a></li>
                 <li><a className="link" href="#inicio">Início</a></li>
             </ul>
-
-            <ul className="lateralNavbar">
-            <a href="#banner">
-                    <img src={logo} alt="Logo Elementus" className="img"/>
-                </a>
-                <li><a className="link" href="#contato">Contato</a></li>
-                <li><a className="link" href="#conteudo">Conteúdo</a></li>
-                <li><a className="link" href="#depoimentos">Depoimentos</a></li>
-                <li><a className="link" href="#servicos">Serviços</a></li>
-                <li><a className="link" href="#inicio">Início</a></li>
-            </ul>
             
+            <ul id="lateralNavbarId" className="lateralNavbar">
+                <li><a className="linkLateral" href="#inicio">Início</a></li>
+                <li><a className="linkLateral" href="#servicos">Serviços</a></li>
+                <li><a className="linkLateral" href="#depoimentos">Depoimentos</a></li>
+                <li><a className="linkLateral" href="#conteudo">Conteúdo</a></li>
+                <li><a className="linkLateral" href="#contato">Contato</a></li>
+            </ul>
+
             <div className="botao">
                 <button onClick={lateralNavbar} className="actionBotao"></button>
                 <img className="imgBotao" src={botao} alt=""/>
