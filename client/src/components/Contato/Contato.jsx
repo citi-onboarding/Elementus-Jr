442287ecfa3d662b251f7a82e060321af03739fb
Giovaniavs <gavs2@cin.ufpe.br>
2020-07-20 12:36:04 -0300

    Base feita - Alguns bugs remanescentes

--- client/src/components/Contato/Contato.jsx
+++ client/src/components/Contato/Contato.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./Contato.css";
 import imgContato from "./Contato.png";   
+import axios from "axios";
+
 
 function Contato(){
+    async function sendMail() {
+        const nome = document.getElementById('inputName').value
+        const nomeEmpresa = document.getElementById('inputNameEmpresa').value
+        const email = document.getElementById('inputEmail').value
+        const telefone = document.getElementById('inputTelefone').value
+        const mensagem = document.getElementById('inputMensagem').value
+        const req = {
+            nome: nome,
+            nomeEmpresa: nomeEmpresa,
+            email: email,
+            telefone: telefone,
+            mensagem: mensagem,
+        }
+        const res = await axios.post("http://localhost:3001/api/contato", req);
+        console.log(res);
+        };
     return(
         <div id="contato" className="background">
             <div className="background1">
@@ -15,13 +33,13 @@ function Contato(){
                     <p className="descricao3">Após enviada a mensagem, um de nossos consultores irá entrar em contato o mais rápido possível.</p>
                     
                     <div className="caixaInput">
-                        <input className="inputName" type="text" placeholder="Nome"/>
-                        <input className="inputNameEmpresa" type="text" placeholder="Nome da Empresa"/>
-                        <input className="inputEmail" type="email" placeholder="E-mail"/>
-                        <input className="inputTelefone" type="Number" placeholder="Telefone"/>
-                        <textarea className="inputMensagem" type="Textarea" placeholder="Mensagem"/>
+                        <input id="inputName" className="inputName" type="text" placeholder="Nome"/>
+                        <input id="inputNameEmpresa" className="inputNameEmpresa" type="text" placeholder="Nome da Empresa"/>
+                        <input id="inputEmail" className="inputEmail" type="email" placeholder="E-mail"/>
+                        <input id="inputTelefone" className="inputTelefone" type="Number" placeholder="Telefone"/>
+                        <textarea id="inputMensagem" className="inputMensagem" type="Textarea" placeholder="Mensagem"/>
                     </div>
-                    <button className="buttonEnviar">Enviar</button>
+                    <button onClick={sendMail}className="buttonEnviar">Enviar</button>
                 </div>
             </div>
         </div>
