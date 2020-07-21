6b532626adda3089b69fcad14cd6b6678b6c1d6e
Giovaniavs <gavs2@cin.ufpe.br>
2020-07-21 17:48:31 -0300

    configurando o deploy

---
+++ public/precache-manifest.9ec172919088474a54278bcad8b1feaf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b69e51185c74a44c3d3feb35ef5f89b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55257fb351ff508642e1",
+    "url": "/static/css/main.a9def0f3.chunk.css"
+  },
+  {
+    "revision": "031b9b236363a7bfb349",
+    "url": "/static/js/2.1f8cc337.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1f8cc337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55257fb351ff508642e1",
+    "url": "/static/js/main.d7824f3c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c59d089f431c43aa18cc8c2427e5e2dc",
+    "url": "/static/media/Contato.c59d089f.png"
+  },
+  {
+    "revision": "fdae531a9366ef30ae4f9445f7c7da4a",
+    "url": "/static/media/Facebook-logo.fdae531a.svg"
+  },
+  {
+    "revision": "6a60a63d1ed198b93abd31da4ae32445",
+    "url": "/static/media/Instagram-logo.6a60a63d.svg"
+  },
+  {
+    "revision": "f88f6247eaf765d5cd6936b11b784ac4",
+    "url": "/static/media/Linkedin-logo.f88f6247.svg"
+  },
+  {
+    "revision": "42b8b677ece2877a9a22b49c8b4a0501",
+    "url": "/static/media/arrow.42b8b677.svg"
+  },
+  {
+    "revision": "c0e1fec30c11a85778ba7e8ad2129747",
+    "url": "/static/media/citi.c0e1fec3.svg"
+  },
+  {
+    "revision": "b5e8bae618610cef5052fa32d76a2e2d",
+    "url": "/static/media/code.b5e8bae6.svg"
+  },
+  {
+    "revision": "1289f1bc372b7952f3c863cd2793bf27",
+    "url": "/static/media/email-icon.1289f1bc.svg"
+  },
+  {
+    "revision": "d9f3a75e0407b5e6e74204902a7b7fee",
+    "url": "/static/media/heart.d9f3a75e.svg"
+  },
+  {
+    "revision": "47a5680c49b1ecb0306e44095bee4c36",
+    "url": "/static/media/phone-icon.47a5680c.svg"
+  }
+]);
