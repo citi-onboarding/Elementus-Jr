1f0b4b238b7fd390db0ca26a1f2db1fdaf2f32ff
Gerardo Majella <alfa@cin.ufpe.br>
2020-07-21 23:27:01 -0300

    feature(depoiment cards): quote image added

--- client/src/components/DepoimentsCard/DepoimentsCard.jsx
+++ client/src/components/DepoimentsCard/DepoimentsCard.jsx
@@ -5,7 +5,7 @@ import Button from '..\\Button\\Button'
 import instagram_logo from ".\\img\\Instagram-logo.svg"
 import facebook_logo from ".\\img\\Facebook-logo.svg"
 import linkedin_logo from ".\\img\\Linkedin-logo.svg"
-
+import quote_img from ".\\img\\quote.svg"
 
 function DepoimentsCard() {
     const [Depoiments, setDepoiments] = useState([]);
@@ -26,6 +26,9 @@ function DepoimentsCard() {
             {Depoiments?.map(({ _id, ClientName, CompanyFunction, Depoiment, Facebook, Linkedin, Instagram, image }) => (
                 <div key={_id} className="container-dc">
                     <div className="card-dc">
+                        <div className="quote-box">
+                            <img src= {quote_img} className="quote-img" />
+                        </div>
                         <div className='yellow-box-dc-container'>
                             <div className="yellow-box-dc">
                                 <button className="button-dc" onClick={() => window.open(Facebook)}><img src={facebook_logo} className="img-social-media-dc" /></button>
