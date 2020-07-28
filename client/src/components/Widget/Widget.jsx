d206283212be09514bf8ac5afecd6b7910238643
Giovaniavs <gavs2@cin.ufpe.br>
2020-07-28 10:50:53 -0300

    Feature( Box de envio de mensagens funcionando / Falta apenas a estilizacao final  )

--- client/src/components/Widget/Widget.jsx
+++ client/src/components/Widget/Widget.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Widget.css";
 import chatbox from "./conversation.svg"
 
 function Widget(){
-    const NumeroTelefone = "https://wa.me/558192500616"
+    const number = "558192500616";
+    const [msg, setMsg] = useState("");
+    const target = `https://api.whatsapp.com/send?phone=${encodeURIComponent(number)}&text=${encodeURIComponent(msg)}`;
+    
+    const SalvarMensagem = (e)=>{
+        setMsg(e.target.value)
+    };
+
     return(
         <div className="backgroundWidget">
-            <button className="buttonWidget" onClick={()=> window.open(NumeroTelefone)}> <img src={chatbox} alt=""/> </button>
+            <div className="widgetTextBox">
+                <h1 className="TituloTextBox">Há algo em que podemos te ajudar? Entre em contato conosco via WhatsApp</h1>
+                <div className="Msg-Button-Textbox">
+                    <textarea onChange={SalvarMensagem} className="MsgTextBox" placeholder="Mensagem"></textarea>
+                    <button onClick={()=> window.open(target)} className="ButtonTextBox">
+                        Enviar
+                    </button>
+                </div>
+            </div>
+            <button className="buttonWidget" /*onClick={()=> window.open(target)}*/> <img src={chatbox} alt=""/> </button>
         </div>
     )
 };
