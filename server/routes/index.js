8526deeb278794e0c3b9210c758a9d1163f87e91
labprogsam <som3@cin.ufpe.br>
2020-09-02 12:47:49 -0300

    Hotfix(widget): Refactored the code and added new field on InfoEmpresa model

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,10 +14,6 @@ const InfoEmpresa = keystone.list("InfoEmpresa");
 module.exports = (app) => {
   app.use(cors());
 
-  // app.get('/', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '../public/index.html'));
-  // });
-
   app.get('/api/services', (req, res) => {
     Service.model.find((err, items) => {
       if (err) return res.apiError('database error', err);
